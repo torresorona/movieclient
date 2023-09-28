0dff01ab7c3aba944d420228eedc1d991ef62662
Carlos Torres Orona <ce.torresorona@gmail.com>
2023-09-28 11:58:33 -0400

    fixed baseURL declaration

--- src/API/axiosConfig.js
+++ src/API/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseUrl : process.env.baseURL || "http://localhost:3000"
+    baseURL : process.env.baseURL || "http://localhost:3000"
 });
